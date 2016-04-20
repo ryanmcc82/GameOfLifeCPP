186e5e8a060988c1838d9b63f0fb837a00e033f2
Ryan McConn <ryanmcc@uab.edu>
2016-04-19 19:38:09 -0500

    playing

---
+++ build2/CMakeFiles/GOFCpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/GOFCpp.dir/GOFCpp.o"
+  "GOFCpp.pdb"
+  "GOFCpp"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/GOFCpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ build2/CMakeFiles/GOLCppReduction.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/GOLCppReduction.dir/GOLCppReduction.o"
+  "GOLCppReduction.pdb"
+  "GOLCppReduction"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/GOLCppReduction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
